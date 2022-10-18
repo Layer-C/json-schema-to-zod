a684018dd072e1039d3217ab196c5e1ef96461af
Marcus Lee <marcustutorial@hotmail.com>
2022-10-18 11:12:32 +0800

    added support for error messages

--- src/parsers/parseString.ts
+++ src/parsers/parseString.ts
@@ -1,9 +1,11 @@
 import { JSONSchema7 } from "json-schema";
 
 export const parseString = (schema: JSONSchema7 & { type: "string" }) => {
-  let r = "z.string()";
+  // @ts-ignore
+  let r = `z.string(${(schema.message && schema.message.required) ? `{ required_error: ${JSON.stringify(schema.message.required)} }` : ''})`;
   if (schema.pattern)
-    r += `.regex(new RegExp(${JSON.stringify(schema.pattern)}))`;
+    // @ts-ignore
+    r += `.regex(new RegExp(${JSON.stringify(schema.pattern)})${(schema.message && schema.message.pattern) ? `, ${JSON.stringify(schema.message.pattern)}` : ''})`;
   if (schema.format === "email") r += ".email()";
   else if (schema.format === "uri") r += ".url()";
   else if (schema.format === "uuid") r += ".uuid()";
